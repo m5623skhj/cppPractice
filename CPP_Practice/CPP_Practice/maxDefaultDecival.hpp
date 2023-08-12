1f11007aca3c280265c194bb71e286de5ee79472
m5623skhj <m5623skhj@gmail.com>
2023-08-12 22:03:08 +0900

    * declval 예제 추가

---
+++ CPP_Practice/CPP_Practice/maxDefaultDecival.hpp
@@ -0,0 +1,7 @@
+#include <utility>
+
+template <typename T1, typename T2, typename RT = std::decay_t<decltype(true ? std::declval<T1>() : std::declval<T2>())>>
+RT Max(T1 a, T2 b)
+{
+	return b < a ? a : b;
+}
