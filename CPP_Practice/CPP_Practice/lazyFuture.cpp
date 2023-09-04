0924a895321c07e1415ea062e8f179e6926c39c2
m5623skhj <m5623skhj@gmail.com>
2023-09-05 00:04:39 +0900

    * Future 예제 추가

---
+++ CPP_Practice/CPP_Practice/lazyFuture.cpp
@@ -0,0 +1,89 @@
+#include "Headers.h"
+#include <coroutine>
+
+template<typename T>
+struct MyFuture
+{
+	struct promise_type;
+	using handle_type = coroutine_handle<promise_type>;
+
+	handle_type coro;
+
+	MyFuture(handle_type h):
+		 coro(h)
+	{
+		cout << " MyFuture::MyFuture" << endl;
+	}
+
+	~MyFuture()
+	{
+		cout << " MyFuture::~MyFuture" << endl;
+		if (coro != nullptr)
+		{
+			coro.destroy();
+		}
+	}
+
+	T get()
+	{
+		cout << " MyFuture::get" << endl;
+		coro.resume();
+		return coro.promise().result;
+	}
+
+	struct promise_type
+	{
+		shared_ptr<T> ptr = make_shared<T>();
+		promise_type()
+		{
+			cout << " promise_type::promise_type" << endl;
+		}
+
+		~promise_type()
+		{
+			cout << " promise_type::~promise_type" << endl;
+		}
+
+		MyFuture<T> get_return_object()
+		{
+			cout << " promise_type::get_return_object" << endl;
+			return MyFuture(handle_type::from_promise(*this)};
+		}
+
+		void return_value(T v)
+		{
+			cout << " promise_type::return_value" << endl;
+		}
+
+		suspend_never initial_suspend()
+		{
+			cout << " promise_type::initial_suspend" << endl;
+			return {};
+		}
+
+		suspend_never final_suspend() noexcept
+		{
+			cout << " promise_type::final_suspend" << endl;
+			return {};
+		}
+
+		void unhandled_exception()
+		{
+			exit(1);
+		}
+	};
+};
+
+MyFuture<int> CreateFuture()
+{
+	cout << "CreateFuture" << endl;
+	co_return 2021;
+}
+
+int main()
+{
+	auto future = CreateFuture();
+	auto res = future.get();
+
+	cout << "res : " << res << endl;
+}
