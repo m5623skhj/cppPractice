bec7c05c02b959b1b034a969c17bd9a7f9cc3894
m5623skhj <m5623skhj@gmail.com>
2024-03-20 00:09:59 +0900

    * 템플릿 함수 호출 모호성 예제 추가

--- CPP_Practice/CPP_Practice/UniversalReferenceTest.cpp
+++ CPP_Practice/CPP_Practice/UniversalReferenceTest.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <utility>
+#include <type_traits>
 
 void Func_Result(int&& _)
 {
@@ -30,6 +31,22 @@ void Func(Args&&... args)
     CallFuncResult(std::forward<Args>(args)...);
 }
 
+template <typename T>
+concept IsConstLValue = std::is_lvalue_reference_v<T> && std::is_const_v<std::remove_reference_t<T>>;
+
+template <typename... Args>
+requires(IsConstLValue<Args> && ...)
+void OverloadTestFunc(Args const&... args)
+{
+    std::cout << "Func(lvalue const Arg&... args) called" << std::endl;
+}
+
+template <typename... Args>
+void OverloadTestFunc(Args&&... args)
+{
+    std::cout << "Func(Arg&&... args) called" << std::endl;
+}
+
 int main()
 {
     int x = 5;
@@ -37,5 +54,26 @@ int main()
     Func(10, 20);
     Func(x, y);
     Func(30, std::move(x));
+
+    const int constItem1 = 0;
+    const int constItem2 = 1;
+    int item1 = 0;
+    int item2 = 0;
+
+    // OverloadTestFunc(constItem1);를 주석처리하고
+    // OverloadTestFunc<const int&>(constItem1);만 호출하면
+    // Func(lvalue const Arg&... args) called가 출력되는데
+    // 주석을 해제하면 
+    // 둘 다 Func(Arg&&... args) called가 출력됨
+    // 함수 호출 모호성 때문인거 같음
+    OverloadTestFunc(constItem1);
+    OverloadTestFunc(constItem1, constItem2);
+
+    OverloadTestFunc(1, 2);
+    OverloadTestFunc(item1);
+    OverloadTestFunc(item1, item2);
+
+    std::cout << std::endl << std::endl;
+
     return 0;
 }
